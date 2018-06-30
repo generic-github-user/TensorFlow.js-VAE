9f837317632cd5aca492b1917fb6412ee6b205b2
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2018-06-30 08:48:26 -0400

    Revert "Add files via upload"
    This reverts commit 6b6f13e07a4b8834b100fea4d0ed99b33d70e897.

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// Define settings for variational autoencoder
-// Size of input and output images in pixels (width and height)
 const imageSize = 16;
-// Number of images to use when training the neural network
-const numTrainingImages = 46;
-// Number of thumbnail canvases to display randomly generated images on
-const numCanvases = 2;
+const trainingImages = 46;
 
-// Automatically generated settings and parameters
-// Volume of image data, calculated by squaring imageSize to find the area of the image (total number of pixels) and multiplying by three for each color channel (RGB)
 const imageVolume = (imageSize ** 2) * 3;
-// Value to multiply pixels by to scale values from 0 - 255 to 0 - 1
 const pixelMul = tf.scalar(1 / 255);
 
 canvas = {
@@ -29,6 +21,20 @@ canvas.output.width = imageSize;
 canvas.output.height = imageSize;
 canvas.reconstruction.width = imageSize;
 canvas.reconstruction.height = imageSize;
+
+const canvases = [];
+for (var i = 0; i < 2; i ++) {
+	var element = document.createElement("canvas");
+	element.id = "canvas-" + i;
+	element.className = "thumbnail";
+	document.body.appendChild(element);
+	canvases.push({
+		"canvas": document.getElementById("canvas-" + i),
+		"parameters": tf.randomNormal([1, 5], -10, 10)
+	});
+	canvases[i].context = canvases[i].canvas.getContext("2d");
+}
+
 
 const encoder = {
 	input: tf.input({shape: imageVolume}),
@@ -104,23 +110,17 @@ const calculateLoss =
 	}
 );
 
-// Create object to store training data in image, pixel, and tensor format
 const trainingData = {
-	// Store training data image elements
 	"images": [],
-	// Store training data as raw arrays of pixel data
 	"pixels": []
 }
-// Add training data to trainingData.images array as an HTML image element
-// Loop through each training image
-for (var i = 0; i < numTrainingImages; i ++) {
-	// Create a new HTML image element with the specified dimensions and set current array index to this element (array.push does not work here)
+for (var i = 0; i < trainingImages; i ++) {
 	trainingData.images[i] = new Image(imageSize, imageSize);
 }
 
 trainingData.images[trainingData.images.length - 1].onload = function () {
 	var pixels;
-	for (var i = 0; i < numTrainingImages; i ++) {
+	for (var i = 0; i < trainingImages; i ++) {
 		pixels = tf.fromPixels(trainingData.images[i], 3);
 		pixels = tf.image.resizeBilinear(pixels, [imageSize, imageSize]);
 		pixels = pixels.dataSync();
@@ -151,29 +151,18 @@ trainingData.images[trainingData.images.length - 1].onload = function () {
 		return tf.tensor(values, [imageSize, imageSize, 3], "int32");
 	}
 
-	// Create thumbnail canvases to render randomly generated images
-	// Create array to store thumbnail canvas elements
 	const canvases = [];
 	const min = encoder.model.predict(trainingData.tensor).min().dataSync()[0];
 	const max = encoder.model.predict(trainingData.tensor).max().dataSync()[0];
-	// Create a specified number of new canvases
-	for (var i = 0; i < numCanvases; i ++) {
-		// Create a new HTML canvas element
+	for (var i = 0; i < 4; i ++) {
 		var element = document.createElement("canvas");
-		// Add a corresponding id property to the canvas element
 		element.id = "canvas-" + i;
-		// Set the "thumbnail" CSS class for the new canvas
 		element.className = "thumbnail";
-		// Add the canvas element to the page body
 		document.body.appendChild(element);
-		// Add this canvas element to the canvases array
 		canvases.push({
-			// Select the canvas element by id and add it to the array
 			"canvas": document.getElementById("canvas-" + i),
-			// Add randomly generated latent space variables for this canvas
-			"variables": tf.randomNormal([1, 5], -10, 10)
+			"parameters": tf.randomUniform([1, 5], min, max)
 		});
-		// Add rendering context object for the canvas
 		canvases[i].context = canvases[i].canvas.getContext("2d");
 	}
 
@@ -214,7 +203,7 @@ trainingData.images[trainingData.images.length - 1].onload = function () {
 			const output_ =
 			tf.tidy(
 				() => {
-					return decoder.model.predict(canvases[i].variables)
+					return decoder.model.predict(canvases[i].parameters)
 					.mul(pixelMul)
 					.clipByValue(0, 1)
 					.reshape(
@@ -227,6 +216,6 @@ trainingData.images[trainingData.images.length - 1].onload = function () {
 	}
 	var interval = window.setInterval(train, 100);
 }
-for (var i = 0; i < numTrainingImages; i ++) {
+for (var i = 0; i < trainingImages; i ++) {
 	trainingData.images[i].src = "./Training Data/Characters/" + (i + 1) + ".png";
 }
